21fc23e476189d01c5762b67e3db8e7752f03498
ShankarAdd <shankarraibagi777>
2023-08-20 08:36:29 +0530

    changeColorToGreen

---
+++ DomManipulation/structure/app.js
@@ -0,0 +1,6 @@
+// console.log('Hello git');
+// console.log('This is first commit into main branch');
+const header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+const sc=document.querySelector('h2');
+sc.style.color='green';
