a98f970b5a7100e2a45f97694b6701006eb30ec9
ShankarAdd <shankarraibagi777>
2023-08-22 12:36:33 +0530

    storingDataInLocal

--- DomManipulation/BookingAppointmentApp/main.js
+++ DomManipulation/BookingAppointmentApp/main.js
@@ -6,18 +6,35 @@ const userList=document.querySelector('#users');
 
 myform.addEventListener('submit', onSubmit);
 
+// function onSubmit(e){
+//     e.preventDefault();
+//     if(firstName.value === ''|| firstEmail.value ===''){
+//         msg.classList.add('error');
+//         msg.innerHTML=('Please enter all fields');
+//         setTimeout(() =>msg.remove(),3125);
+//     }else{
+//         const li=document.createElement('li');
+//         li.appendChild(document.createTextNode(`${firstName.value} : ${firstEmail.value}`));
+//         userList.appendChild(li);
+//         //Clear fields
+//         firstName.value='';
+//         firstEmail.value='';
+//     }
+// }
+
+
+//STORING DATA IN THE LOCAL STORAGE 
 function onSubmit(e){
     e.preventDefault();
-    if(firstName.value === ''|| firstEmail.value ===''){
+    if(firstName.value===''||firstEmail.value===''){
         msg.classList.add('error');
         msg.innerHTML=('Please enter all fields');
-        setTimeout(() =>msg.remove(),3125);
-    }else{
-        const li=document.createElement('li');
-        li.appendChild(document.createTextNode(`${firstName.value} : ${firstEmail.value}`));
-        userList.appendChild(li);
-
-        firstName.value='';
-        firstEmail.value='';
+        setTimeout(() => msg.remove(),2500);
+    }
+    else{
+        localStorage.setItem('name',firstName.value);
+        
+        localStorage.setItem('email',firstEmail.value);
+        
     }
 }
