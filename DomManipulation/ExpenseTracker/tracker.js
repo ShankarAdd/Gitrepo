0a2d8218b8eec43897ee78ebd18d12bf96056bde
ShankarAdd <shankarraibagi777@gmail.com>
2023-10-27 14:31:44 +0530

    CRUD Using MySQL Database

--- DomManipulation/ExpenseTracker/tracker.js
+++ DomManipulation/ExpenseTracker/tracker.js
@@ -1,56 +1,86 @@
 const myform=document.querySelector('#expenseForm');
-const expense=document.querySelector('#expense');
-const description=document.querySelector('#description');
+const expense1=document.querySelector('#expense');
+const description1=document.querySelector('#description');
 const value1=document.getElementById('choose');
 const addButton=document.querySelector('#clickOnIt');
 const noofUsers=document.querySelector('#users');
+const expens = document.querySelector('#expenseId');
 
 addButton.addEventListener('click', onSubmit);
 
 function onSubmit(){
     //taking data when user fill the form
-    const expenseData=expense.value;
-    const descriptionData=description.value;
-    const text = value1.options[value1.selectedIndex].text;
-    
-    if(expenseData === '' ||descriptionData === '' || text === '' ){
+    const amount=expense1.value;
+    const description=description1.value;
+    const category = value1.options[value1.selectedIndex].text;
+    const expe = expens.value;
+
+    const expense = {
+        amount,
+        description,
+        category,
+    }
+    axios.post("http://localhost:3000/expense/add-expense", expense)
+        .then((response) => showOnScreen(response.data))
+        .catch((err) => {
+            console.log(err);
+    })
+      
+}
+function showOnScreen(expense){
+    if(expense.amount === '' ||expense.description === '' || expense.category === '' ){
         alert('Please enter all fields')
     }
     else{
         //Displaying the data in the page
         const li=document.createElement('li');
-        li.textContent = expenseData + " : " + descriptionData + " : " +text;
+        li.textContent = expense.amount + " : " + expense.description + " : " +expense.category;
         noofUsers.appendChild(li);
-
+    
         //Storing the data in local storage
-        const allData = expenseData + " : " + descriptionData + " : " +text;
-        localStorage.setItem(expenseData, allData);
-
+        //localStorage.setItem(expense.amount, JSON.stringify(expense));
+    
         //adding delete button
         const deleteButton = document.createElement('input');
-		deleteButton.type = 'button';
-		deleteButton.value = 'Delete';
+        deleteButton.type = 'button';
+        deleteButton.value = 'Delete';
         deleteButton.className='btn-outline-danger m-lg-1 delete';
-
+    
         deleteButton.onclick = () =>{
-            localStorage.removeItem(expenseData);
+            //localStorage.removeItem(amount);
+            const id = expense.expe;
+            axios.delete('http://localhost:3000/expense/delete-expense/' + id)
+                .catch((err) => console.log(err));
             noofUsers.removeChild(li);
         }
         li.appendChild(deleteButton);
-
+    
         //edit button to edit
         const editButton = document.createElement('input');
-		editButton.type = 'button';
-		editButton.value = 'Edit';
+        editButton.type = 'button';
+        editButton.value = 'Edit';
         editButton.className='btn-outline-danger m-lg-1 delete';
-
+    
         editButton.onclick = () =>{
-            localStorage.removeItem(expenseData);
-            noofUsers.removeChild(li);
-            document.querySelector('#expense').value=expenseData;
-            document.querySelector('#description').value=descriptionData;
-            document.getElementById('choose').value=text;
+            //localStorage.removeItem(amount);
+                const id1 = expense.id;
+                noofUsers.removeChild(li);
+                document.querySelector('#expense').value=expense.amount;
+                document.querySelector('#description').value=expense.description;
+                document.getElementById('choose').value=expense.category;
+                document.getElementById('expenseId').value=expense.expenseId;
+                axios.delete('http://localhost:3000/expense/delete-expense/' + id1)
+                .catch(err => console.log(err));
         };
         li.appendChild(editButton);
     }
 }
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get("http://localhost:3000/expense/get-expense")
+        .then((response) => {
+            for (let i = 0; i < response.data.length; i++) {
+                showOnScreen(response.data[i]);
+            }
+        })
+        .catch((err) => console.log(err));
+})
